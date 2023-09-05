4b204ac36c36b8d3b662b4158e0bce9010794197
0dot77 <yty0706@gmail.com>
2023-09-05 18:47:37 +0900

    fix : sanity cli

--- sanity-studio/sanity.cli.ts
+++ sanity-studio/sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'l6vl0xt8',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
